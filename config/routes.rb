8c0329fa15f03cf9a402519d9a4e63b0d4da24d4
Charlotte Wegener <charlotteanne.wegener@gmail.com>
2022-02-18 16:09:57 +0100

    new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/movies', to: 'movies#index'
-  get '/movies/:id', to: 'movies#show'
-  get '/movies/:id/edit', to: 'movies#edit'
-  get '/movies/new', to: 'movies#new'
-  patch '/movies/:id', to: 'movies#update'
-  post '/movies', to: 'movies#create'
-  delete '/movies/:id', to: 'movies#destroy'
-  get '/lists', to: 'lists#index'
-  get '/lists/new', to: 'lists#new'
-  get '/lists/:id', to: 'lists#show'
-  post '/lists', to: 'lists#create'
-  get '/bookmarks/new', to: 'bookmarks#new'
-  post '/bookmarks', to: 'bookmarks#create'
-  delete '/bookmarks/:id', to: 'bookmarks#destroy'
+  root to: 'lists#index'
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+    resources :reviews, only: :create
+  end
 end
